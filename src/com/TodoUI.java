5a9f0304be4846b290d915aeea928d48da34e5f8
tony11301130 <tony11301130@gmail.com>
2023-11-01 18:59:37 +0800

    #

--- src/com/TodoUI.java
+++ src/com/TodoUI.java
@@ -21,18 +21,21 @@ public class TodoUI {
         JPanel panel = new JPanel();
         frame.add(panel);
 
-        DefaultListModel<String> todoListModel = new DefaultListModel<>();
-        JList<String> todoList = new JList<>(todoListModel);
+        DefaultListModel<TodoItem> todoListModel = new DefaultListModel<>();
+        JList<TodoItem> todoList = new JList<>(todoListModel);
+        todoList.setCellRenderer(new TodoListCellRenderer());
 
         JTextField newItemField = new JTextField(20);
         JButton addButton = new JButton("Add");
         JButton deleteButton = new JButton("Delete Selected");
+        JButton completeButton = new JButton("Mark as Completed");
 
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String newItem = newItemField.getText();
-                if (!newItem.isEmpty()) {
+                String newItemText = newItemField.getText();
+                if (!newItemText.isEmpty()) {
+                    TodoItem newItem = new TodoItem(newItemText);
                     todoListModel.addElement(newItem);
                     newItemField.setText("");
                 }
@@ -49,11 +52,67 @@ public class TodoUI {
             }
         });
 
+        completeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int selectedIndex = todoList.getSelectedIndex();
+                if (selectedIndex >= 0) {
+                    TodoItem selectedTodo = todoListModel.get(selectedIndex);
+                    selectedTodo.setCompleted(true);
+                    todoList.repaint();
+                }
+            }
+        });
+
         panel.add(newItemField);
         panel.add(addButton);
         panel.add(todoList);
         panel.add(deleteButton);
+        panel.add(completeButton);
 
         frame.setVisible(true);
+    }
+
+    static class TodoItem {
+        private String text;
+        private boolean completed;
+
+        public TodoItem(String text) {
+            this.text = text;
+            this.completed = false;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public boolean isCompleted() {
+            return completed;
+        }
+
+        public void setCompleted(boolean completed) {
+            this.completed = completed;
+        }
+
+        @Override
+        public String toString() {
+            return text;
+        }
+    }
+
+    static class TodoListCellRenderer extends DefaultListCellRenderer {
+        @Override
+        public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+            Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+            if (value instanceof TodoItem) {
+                TodoItem todoItem = (TodoItem) value;
+                if (todoItem.isCompleted()) {
+                    c.setForeground(Color.GREEN);
+                } else {
+                    c.setForeground(Color.BLACK);
+                }
+            }
+            return c;
+        }
     }
 }
